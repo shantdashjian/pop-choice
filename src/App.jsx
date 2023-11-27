259ff1907d744b0cc3c6f815f578c2ba12a0b220
Shant Dashjian <shantgdashjian@gmail.com>
2023-11-27 22:18:16 +0400

    Add React components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { openai, supabase } from './config.js'
+import Header from './components/Header.jsx'
+import Main from './components/Main.jsx'
 
 function App() {
 
   return (
-    <>
-      Hello PopChoice
-    </>
+    <div className="container">
+      <Header />
+      <Main />
+    </div>
   )
 }
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <div className="main-container">
+      Main
+    </div>
+  )
+} 
